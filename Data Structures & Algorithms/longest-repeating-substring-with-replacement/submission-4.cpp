bae193efe1dad18109f689829f429a366af5d084
Daivik Shaknar Gokhale <119348669+dedwiks@users.noreply.github.com>
2026-09-08 00:35:47 +0530

    Add: longest-repeating-substring-with-replacement - submission-4

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-4.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        unordered_set<char> charset(s.begin(), s.end());
+        int res = 0;
+
+        for (const auto& c : charset) {
+            int l = 0, count = 0;
+
+            for (int r = 0; r < s.size(); r++) {
+                if (s[r] == c) {
+                    count++;
+                }
+
+                while ((r - l + 1) - count > k) {
+                    if (s[l] == c) {
+                        count--;
+                    }
+                    l++;
+                }
+                res = max(res, r - l + 1);
+            }
+        }
+        return res;
+    }
+};
